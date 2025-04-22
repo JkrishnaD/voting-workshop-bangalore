902c0da36c30b57ce13e91c414f6c8ec08b818f5
JKrishnaD <jayak5063@gmail.com>
2025-04-22 21:35:46 +0530

    fixes

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -2,7 +2,7 @@
 
 use anchor_lang::prelude::*;
 
-declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+declare_id!("4VJ8dXrKwYYgmcX3egWmdN9mAjLLjWT2nqpLHFPG7D9S");
 
 #[program]
 pub mod voting {
